f7e33655148cf71a36336fac50c8527cd9b61482
cristhian gonzalez <cristhianmg1122@gmail.com>
2021-09-29 00:50:30 -0500

    Se realizo la create de paciente

--- HospiEnCasa.App.Frontend/Pages/Pacientes/Create.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Create.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
 namespace HospiEnCasa.App.Frontend.Pages.Pacientes
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioPaciente _repoPaciente;
+        public Paciente paciente {get;set;}
+        public CreateModel(IRepositorioPaciente repoPaciente)
+        {
+            _repoPaciente = repoPaciente;
+        }
         public void OnGet()
         {
+            paciente = new Paciente();
         }
     }
 }
